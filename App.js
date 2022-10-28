a1852803a6d84ecfc07418265d9c6342d46a2ece
jinwonccie <jinwonccie>
2022-10-28 17:35:25 +0900

    10-28-3

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집'
-
+  let post = '강남 우동 맛집';
+  let [글제목0, a] = useState('남자겨울코트추천');
+  let [글제목1, b] = useState('여자겨울코트추천');
+  let [글제목2, c] = useState('파이션 독학');
+  let [logo, setLogo] = useState('React Blog');
   return (
+
+
     <div className="App">
       <div className="black-nav">
-        <h4>블로그</h4>
+        <h4>{logo}</h4>
       </div>
-        <h4>{post}</h4>
+      <div className="list">
+        <h4>{글제목0}</h4>
+        <p>2월17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목1}</h4>
+        <p>2월17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목2}</h4>
+        <p>2월17일 발행</p>
+      </div>
     </div>
-  );
+
+
+);
 }
 
 export default App;
